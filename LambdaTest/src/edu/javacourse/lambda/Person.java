24f7571b0fad623953fb21404a174509db3486cc
Антон Сабуров <Anton.Saburov@ocrv.ru>
2016-02-29 17:57:28 +0300

    Lambda example

---
+++ LambdaTest/src/edu/javacourse/lambda/Person.java
@@ -0,0 +1,61 @@
+package edu.javacourse.lambda;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Person {
+
+    public enum Sex {
+        MALE, FEMALE
+    }
+
+    String name;
+    Date birthday;
+    Sex gender;
+    String emailAddress;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Sex getGender() {
+        return gender;
+    }
+
+    public void setGender(Sex gender) {
+        this.gender = gender;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "name=" + name + ", birthday=" + birthday + ", gender=" + gender + ", emailAddress=" + emailAddress + '}';
+    }
+
+    public int getAge() {
+        return Calendar.getInstance().get(Calendar.YEAR);
+    }
+    
+    public void printPerson() {
+        System.out.println(this.toString());
+    }
+}
