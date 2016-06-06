3dab7aa171cf3643e442ce27f35a9d31966115ec
Антон Сабуров <Anton.Saburov@ocrv.ru>
2016-06-06 18:10:43 +0300

    Proxy is added

--- JarLoader/string-generator-main/src/edu/javacourse/ref/JarFileLoader.java
+++ JarLoader/string-generator-main/src/edu/javacourse/ref/JarFileLoader.java
@@ -1,5 +1,8 @@
 package edu.javacourse.ref;
 
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLClassLoader;
@@ -24,5 +27,21 @@ public class JarFileLoader extends URLClassLoader {
         System.out.println("CLASS:" + lc.getCanonicalName());
         StringGenerator gen = (StringGenerator) lc.newInstance();
         System.out.println(gen.generate());
+
+        MyInvocationHandler handler = new MyInvocationHandler();
+        StringGenerator proxy = (StringGenerator) Proxy.newProxyInstance(
+                            StringGenerator.class.getClassLoader(),
+                            new Class[] { StringGenerator.class },
+                            handler);
+        String answer = proxy.generate();
+        System.out.println("ANSWER:" + answer);
+    }
+}
+
+class MyInvocationHandler implements InvocationHandler {
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        System.out.println("Method:" + method.getName());
+        return null;
     }
 }
